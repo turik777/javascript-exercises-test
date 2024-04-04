9933ad93c9da6a0303b57c4bf9e9a0c432b834e3
turik777 <52492191+turik777@users.noreply.github.com>
2024-04-05 01:24:50 +0300

    Complete function repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let resultString = "";
+    if (num < 0) return "ERROR";
+    for (let i = 0; i < num; i++) {
+        resultString += string;
+    }
+    return resultString;
 };
 
 // Do not edit below this line
