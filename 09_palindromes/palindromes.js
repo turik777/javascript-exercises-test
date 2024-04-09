c35732339d5a9f27a8483d674c983a1aca10fcdc
turik777 <52492191+turik777@users.noreply.github.com>
2024-04-09 16:08:40 +0300

    Complete palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let string = str.split(" ").join("").replace(/[!,.]/g, "").toLowerCase();
+    let reversedString = string.toLowerCase().split("").reverse().join("");
+    return string === reversedString;
 };
 
 // Do not edit below this line
