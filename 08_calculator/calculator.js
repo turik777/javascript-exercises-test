05a0f56680320ea24ed6a0250e68a9a7ae135b51
turik777 <52492191+turik777@users.noreply.github.com>
2024-04-09 03:55:17 +0300

    Complete factorial function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,8 +18,13 @@ const power = function(a, b) {
 	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) return 1;
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
