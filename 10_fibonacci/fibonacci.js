3ed0b26e39188bbf6aa1e54756837820c02f7eb7
turik777 <52492191+turik777@users.noreply.github.com>
2024-04-09 16:52:04 +0300

    Complete fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n < 0) return "OOPS";
+    let a = 0;
+    let b = 1;
+    let c = a + b;
+    if (n === 0) return a;
+    for (let i = 2; i < n; i++) {
+        a = b;
+        b = c;
+        c = a + b;
+    }
+    return c;
 };
 
 // Do not edit below this line
