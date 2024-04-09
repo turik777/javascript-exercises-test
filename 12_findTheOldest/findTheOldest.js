e1aec7c5d517d4084371a9e4bc714bd58db21a09
turik777 <52492191+turik777@users.noreply.github.com>
2024-04-09 17:56:20 +0300

    Complete findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,6 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.map(person => !person.yearOfDeath ? person.yearOfDeath = new Date().getFullYear() : person.yearOfDeath)
+    return people.sort((personA, personB) => (personB.yearOfDeath - personB.yearOfBirth) - (personA.yearOfDeath - personA.yearOfBirth))[0];
 };
 
 // Do not edit below this line
